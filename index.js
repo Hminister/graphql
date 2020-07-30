3584e7b38ef6ced4230aca7abf2adf3cf647aa16
Hminister <861592174@qq.com>
2020-07-30 18:13:55 +0800

    入门提交

---
+++ index.js
@@ -0,0 +1,48 @@
+import Koa from 'koa'
+import Router from 'koa-router'
+import graphlHTTP from 'koa-graphql'
+import { buildSchema } from 'graphql'
+import { nanoid } from 'nanoid'
+
+const app = new Koa()
+const router = new Router()
+const schema = buildSchema(`
+  type User {
+    id: ID!,
+    name: String,
+    age: Int
+  }
+  type Query {
+    user: User,
+    hello: String,
+  }
+`)
+const rootValue = {
+  hello: () => `hello World`,
+  user: () => ({
+    id: nanoid(),
+    name: 'jkljf',
+    age: 18,
+  }),
+}
+
+router.all(
+  '/graphql',
+  graphlHTTP({
+    schema,
+    rootValue,
+    graphiql: true,
+  })
+)
+
+/* 传统的restful写法
+  router.get('/', async (ctx) => {
+    ctx.body = `<h1>hello word</h1>`
+  })
+*/
+
+app.use(router.routes(), router.allowedMethods())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`server start at ${PORT}`))
